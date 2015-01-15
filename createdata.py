478be98c7caf1017284d929e8fc9931f37c7820f
Hanno Rein <hanno@hanno-rein.de>
2015-01-15 13:57:27 -0500

    n

--- createdata.py
+++ createdata.py
@@ -1,9 +1,10 @@
 # Import the rebound module
 import rebound
 import random
+import math
 
 rebound.particle_add( m=1. )                    # Star
-rebound.particle_add( m=1.2345e-3, x=1., vy=1. )     # Planet
+rebound.particle_add( m=3.2345e-3, x=1.5, vy=math.sqrt(1./1.5) )     # Planet
 rebound.move_to_center_of_momentum()
 
 N = rebound.get_N()
--- radialvelocity_4.py
+++ radialvelocity_4.py
@@ -1,4 +1,9 @@
 # Import the rebound module
+import numpy as np
+data = np.loadtxt("data.txt")
+
+
+
 import rebound
 
 rebound.particle_add( m=1. )                    # Star
@@ -13,11 +18,12 @@ vr = []
 
 while rebound.get_t()<30.:
     rebound.step()
-    t.append(rebound.get_t())
-    vr.append(particles[0].vx)
+    t.append(rebound.get_t()*58.130101)     # to days
+    vr.append(particles[0].vx*29785.891)    # to m/s
 
 import matplotlib; matplotlib.use("pdf")
 import matplotlib.pyplot as pl
 
-pl.plot(t,vr)
+pl.plot(t,vr,"-")
+pl.plot(data.T[0],data.T[1],".")
 pl.savefig("orbit.pdf")
